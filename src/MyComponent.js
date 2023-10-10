85f5cf9aaebb3efcf4e8e1c8dfb42da304cd2916
alswns6327 <alswns6327@naver.com>
2023-10-10 21:49:38 +0900

    default, props.types 학습

---
+++ src/MyComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component {
+
+    static defaultProps = {
+        name : '기본 이름'
+    }
+
+    static propTypes = {
+        name : PropTypes.string,
+        favoriteNumber : PropTypes.number.isRequired
+    }
+
+    render(){
+        const {name, children, favoriteNumber, a} = this.props;
+        return(
+            <>
+                <div>name : {name + children + a + " " + favoriteNumber}</div>
+            </>
+        )
+    }
+};
+
+// MyComponent.defaultProps = {
+//     name : '기본 이름'
+// };
+
+// MyComponent.propTypes = {
+//     name : PropTypes.string,
+//     favoriteNumber : PropTypes.number.isRequired,
+//     a : PropTypes.oneOf(['ttt', 'aaa']),
+//     b : PropTypes.objectOf(PropTypes.bool)
+// }
+
+export default MyComponent;
