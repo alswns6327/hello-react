87c567a3e60bdab79bd71a7192d99128d4cf5813
alswns6327 <alswns6327@naver.com>
2023-10-17 23:46:49 +0900

    map

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map((name, index)=> <li key={index}>{name}</li>);
+    const [names, setNames] = useState([
+        {id : 1, text : '눈사람'},
+        {id : 2, text : '얼음'},
+        {id : 3, text : '눈'},
+        {id : 4, text : '바람'},
+    ]);
+
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const nameList = names.map(val=><li key={val.id}>{val.text}</li>)
+
+    const onChange = e => setInputText(e.target.value);
+
+    const onClick = ()=> {
+        
+        const nextNames = names.concat([{
+            id : nextId, text : inputText
+        }])
+        
+        setNames(nextNames);
+        setNextId(nextId+1);
+        setInputText('');
+    }
 
     return (
-        <ul>{nameList}</ul>
+        <>
+            <input value={inputText} onChange={onChange}/>
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul>
+        </>
     );
 };
 
