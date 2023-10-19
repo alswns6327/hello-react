459b03dcaa5a2f3eb674d9a1d35784deb01f5086
alswns6327 <alswns6327@naver.com>
2023-10-19 23:59:12 +0900

    class component life cycle

---
+++ src/LifeCycleSample.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class LifeCycleSample extends Component {
+
+    state = {
+        number : 0,
+        color : null
+    } 
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default LifeCycleSample;
